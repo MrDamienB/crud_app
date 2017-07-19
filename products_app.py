aa47ab9240de9ddb5cec60e190aa9d9c1dd8cd88
Damien B <MrDamienB@users.noreply.github.com>
2017-07-19 18:42:44 -0400

    CP1 prompt user for operation

--- products_app.py
+++ products_app.py
@@ -1,1 +1,19 @@
-print("HELLO")
+
+
+
+print("-----------------------------------","\n","PRODUCTS APPLICATION", '\n'"-----------------------------------")
+print("Welcome mrdamienb")
+
+menu = """
+    There are 20 products
+
+    Please choose an operation:
+
+    'List', 'Show', 'Create', 'Update', 'Destroy'
+
+    """
+
+
+chosen_operation = input(menu)
+
+print(chosen_operation)
