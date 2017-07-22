628c9acd6ae6354c78ae021bffd102d836cc4bf8
Damien B <MrDamienB@users.noreply.github.com>
2017-07-22 18:03:46 -0400

    Checkpoint 3 (a couple of errors remain)

--- app/products_app.py
+++ app/products_app.py
@@ -1,4 +1,6 @@
 import csv
+import getpass
+username = getpass.getuser()
 
 products = []
 headers = ["id", "name", "aisle", "department", "price"]
@@ -15,6 +17,7 @@ csv_file_path = "data/products.csv"
 def list_products():
     for product in products:
         print(product["id"], product['name'])
+
 def show_product():
     while True:
         show_request = input("WOULD YOU LIKE TO LOOK UP AN ITEM (Y/N)?: ")
@@ -31,41 +34,85 @@ def show_product():
             else:
                 print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product)
         else: print("Please enter 'Y' for 'yes' or 'N' or 'no'")
+
 def create_product():
     print("PLEASE PROVIDE THE NEW PRODUCT INFORMATION")
     new_product = {"id": creat_new_pid() }
     for header in user_input_headers:
-        new_product[header] = input("The '{0}' is: ".format(header))
-#    product_name = input("new product name:")
-#    product_department = input("new product department:")
-#    product_aisle = input("new product aisle:")
-#    product_price = input("new product price:")
-#    new_product = {
-#        "id": product_id,
-#        "name": product_name,
-#        "aisle": product_aisle,
-#        "department": product_department,
-#        "price": product_price
-#    }
-    print("Review the new product you added:","\n",new_product)
-    products.append(new_product)
-#    print(products)
+        new_product[header] = input("The '{0}' is: ".format(header).title())
+    print("Please review and CONFIRM the information for the new product you added:",
+        "\n", "        ID #: ",new_product["id"],
+        "\n", "NAME @ PRICE: ",new_product["name"].title(), "@" ,'${0:.2f}'.format(float(new_product["price"])),
+        "\n", "       AISLE: ",new_product["aisle"].title(),
+        "\n", "  DEPARTMENT: ",new_product["department"].title())
+    confirmation = input("Would you like to save your changes? (Y/N): ")
+    if confirmation == "Y":
+        products.append(new_product)
+        print("Your changes have been saved!")
+    elif confirmation == "N": print("Sorry about that, let's try again.")
+    else: print("Please enter 'Y' for 'yes' or 'N' or 'no'")
+    print("There are now",len(products), "unique products in your inventory.")
+
 def update_product():
-    print("UPDATING A PRODUCT")
+    while True:
+        show_request = input("Would you like to UPDATE an item? (Y/N): ")
+        if show_request == "N": break
+        elif show_request == "Y":
+            lookup = input("PLEASE ENTER THE PRODUCT ID OF THE PRODUCT YOU ARE UPDATING: ")
+            product = [p for p in products if p["id"] == lookup][0]
+            if product:
+                print("\n","PLEASE ENTER THE PRODUCTS UPDATED INFORMATION", "\n")
+                for header in user_input_headers:
+                    product[header] = input("Change '{0}' from '{1}' to: ".format(header.title(), product[header]))
+                print("\n","REVIEW UPDATED PRODUCT INFO:",
+                "\n", "        ID #: ",product["id"],
+                "\n", "NAME @ PRICE: ",product["name"].title(), "@" ,'${0:.2f}'.format(float(product["price"])),
+                "\n", "       AISLE: ",product["aisle"].title(),
+                "\n", "  DEPARTMENT: ",product["department"].title(),
+                "\n", "\n", "If this is incorrect, please start over from the main menu.", "\n")
+            else:
+                print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product)
+        else: print("Please enter 'Y' for 'Yes' or 'N' or 'No'")
+
 def destroy_product():
-    print("DESTROYING A PRODUCT")
+    while True:
+        show_request = input("WOULD YOU LIKE TO DELETE A PRODUCT? (Y/N): ")
+        if show_request == "N": break
+        elif show_request == "Y":
+            lookup = input("ENTER ID: ")
+            product = [p for p in products if p["id"] == lookup][0]
+            if product:
+                while True:
+                    print("Please confirm you'd like to PERMANENTLY DELETE:", "\n","\n",
+                        "ID #:",product["id"],"\n",
+                        "NAME:",product["name"].title(),"\n",)
+                    confirmation = input("'Y' to CONFIRM, 'N' to ABORT: ")
+                    if confirmation == "N": break
+                    elif confirmation == "Y":
+                        del products[products.index(product)]
+                        print("\n","THE FOLLOWING PRODUCT HAS BEEN PERMANENTLY REMOVED:",
+                        "\n", "        ID #: ",product["id"],
+                        "\n", "NAME @ PRICE: ",product["name"].title(), "@" ,'${0:.2f}'.format(float(product["price"])),
+                        "\n", "       AISLE: ",product["aisle"].title(),
+                        "\n", "  DEPARTMENT: ",product["department"].title(), "\n")
+                        break
+                    else: "Please enter a valid confirmation ('Y' to CONFIRM, 'N' to ABORT): "
+            else:
+                print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", lookup)
+        else: print("Please enter 'Y' for 'yes' or 'N' or 'no':")
 
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
         products.append(row)
-#        print(row["id"], row["name"])
-
-print("-----------------------------------","\n","PRODUCTS APPLICATION", '\n'"-----------------------------------")
-print("Welcome mrdamienb")
 
 menu = """
-    There are {0} products (Exit and reopen app to refresh product count)
+    ------------------------------------
+            PRODUCTS APPLICATION
+    ------------------------------------
+    Hello {0},
+
+    There are {1} products (Exit and reopen app to refresh product count)
 
     Please choose an operation:
     'List'    | Display a list of product identifiers
@@ -74,21 +121,13 @@ menu = """
     'Update'  | Edit an existing product
     'Destroy' | Delete an existing product
 
-    Operation:""".format(len(products))
+    Operation:""".format(username, len(products))
 
-search = """
-You can search via the following search keys:
-{0}
-Please enter your search key:
-""".format(headers)
-
-#new_products = "data/new_products.csv"
-#with open(new_products, "w", newline='') as csv_file:
-#    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
-#    writer.writeheader()
-#    for item in products:
-#        writer.writerow(item)
-#        print(item["id"], item["name"])
+#search = """ I was trying to make the lookup function have an option for any header. I couldn't figure it out.
+#You can search via the following search keys:
+#{0}
+#Please enter your search key:
+#""".format(headers)
 
 while True:
     chosen_operation = input(menu)
